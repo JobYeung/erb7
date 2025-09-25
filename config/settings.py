21d3defcb4267ac89dfa4336f622a947b22ec205
jobyeung <job@yeungshk.com>
2025-09-25 15:02:11 +0800

    step 4 about base

--- config/settings.py
+++ config/settings.py
@@ -140,7 +140,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+# add a STATIC_ROOT
+STATIC_ROOT = os.path.join(BASE_DIR,'static') 
 STATIC_URL = 'static/'
+# add this 
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'config/staticfiles')] 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
