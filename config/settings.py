ea9724c8b5a83f35ec8abd21dc5c607cbd5f7dab
jobyeung <job@yeungshk.com>
2025-09-29 16:15:46 +0800

    Step 8 migration Django and PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -102,10 +102,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clinic',
+        'USER': 'postgres',
+        'PASSWORD': 'aty',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -160,3 +170,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
 "127.0.0.1",
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
