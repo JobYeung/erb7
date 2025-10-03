277ba8ac74e903fc262a2cf29635cbe4adb6213b
jobyeung <job@yeungshk.com>
2025-10-03 17:44:07 +0800

    step 11 about complete

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from listings.models import Listing
+from doctors.models import Doctor
 
 def index(request):
     listings = Listing.objects.filter(is_published=True)[:3]
@@ -7,5 +8,10 @@ def index(request):
     return render(request,'pages/index.html',context)
 
 def about(request):
-    
-    return render(request,'pages/about.html')
+    doctors  = Doctor.objects.order_by('-hire_date')[:3]
+    mvp_doctors = Doctor.objects.all().filter(is_mvp=True)
+    context = {
+        "doctors":doctors,
+        "mvp_doctors":mvp_doctors
+    }
+    return render(request,'pages/about.html',context)
